b31acee418de5d754ddb59d2248423b8705b08f3
Aarush-ICEMAN <146875714+Aarush-ICEMAN@users.noreply.github.com>
2023-11-20 22:50:23 +0530

    Add files via upload

---
+++ Airthmetic Operators.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main(){
+	float a,b,c;
+	printf("Enter the Two Numbers\n");
+	scanf("%f%f",&a,&b);
+	c=a+b;
+	printf("The Sum of Two Numbers=%f\n",c);
+	c=a-b;
+	printf("The Subtraction of Two Numbers=%f\n",c);
+	c=a*b;
+	printf("The Multiplication of Two Numbers=%f\n",c);
+	c=a/b;
+	printf("The Division of Two Numbers=%f\n",c);
+	
+}
---
+++ Arrays .c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+// program for using arrys 
+int marks[5];
+printf("Enter the marks of student 1 \n");
+scanf("%d",&marks[0]);
+
+printf("Enter the marks of student 2 \n");
+scanf("%d",&marks[1]);
+
+ printf("Enter the marks of student 3 \n");
+scanf("%d",&marks[2]);
+
+ printf("Enter the marks of student 4 \n");
+scanf("%d",&marks[3]);
+
+ printf("Enter the marks of student 5 \n");
+scanf("%d",&marks[4]);
+printf("the marks of students is: %d %d %d and %d",marks[0],marks[1],marks[2],marks[3],marks[4]);
+
+
+ 
+
+ 
+ 
+ 
+
+ return 0;
+ 
+ 	
+}
